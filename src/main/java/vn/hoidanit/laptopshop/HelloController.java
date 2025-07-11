12064a76965853dd1214ad6ba9f51952886d6e3e
Cong Minh <cascassnguyen@gmail.com>
2025-07-11 15:52:32 +0700

    vid 43

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,7 +9,16 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with menhhh!";
+        return "Hello World update!";
     }
 
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
+    }
 }
